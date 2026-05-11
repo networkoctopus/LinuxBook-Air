be53617a31c251fa35fa80c1322e6127b75a3b8d
networkoctopus <aidan@networkoctopus.com>
2026-05-11 21:13:40 +0100

    remove sleep 1 from backlight fix

--- build_files/fixes/restore-backlight.sh
+++ build_files/fixes/restore-backlight.sh
@@ -5,7 +5,6 @@ case "$1" in
         cat "$BACKLIGHT/brightness" > /tmp/backlight-save 2>/dev/null || true
         ;;
     post)
-        sleep 1
         [ -f /tmp/backlight-save ] && \
             cat /tmp/backlight-save > "$BACKLIGHT/brightness" 2>/dev/null || true
         ;;
